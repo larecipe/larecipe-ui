5bf7857c1f07f1d4bb92b3b6d51b7ad12d2841aa
Saleem Hadad <saleem.hadad@talabat.com>
2022-09-17 15:02:56 +0400

    fix css output

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.css"],
+  content: ["./src/**/*.js", "../lib/**/*.blade.php"],
   darkMode: 'class',
   theme: {
     extend: {
